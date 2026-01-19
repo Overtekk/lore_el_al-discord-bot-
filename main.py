ce3793a89cb50589a129990175850d10dce32e9f
Overtek <theovertekgaming@gmail.com>
2026-01-19 20:06:26 +0100

    feur

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+#!/usr/bin/env python
+
 import os
 import discord
-import random
-import re
 import database
 from dotenv import load_dotenv
-from discord import app_commands
 from discord.ext import commands
 
 # Get private token
@@ -15,29 +14,21 @@ token = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
 
+# Main class
 class MyClient(commands.Bot):
     def __init__(self, intents):
         super().__init__(command_prefix="!", intents=intents)
 
     async def setup_hook(self):
-        database.create_tables()
-        await self.load_extension("cogs.game")
+        await self.load_extension("cogs.feur")
+        print("Module feur loaded!")
+        # database.create_tables()
+        # await self.load_extension("cogs.game")
+        # print("Module GuessTheNumber loaded!")
         await self.tree.sync()
 
     async def on_ready(self):
         print(f"Bot connected as {self.user}!")
-
-    async def on_message(self, message):
-
-        await self.process_commands(message)
-        message_list = ["feur", "feur !", "feur :)", "feuuuur", "feur..", "c'est feur"]
-
-        if message.author is not self.user:
-            if re.search(r'\bquoi\W*$', message.content.lower()):
-                await message.channel.send(random.choice(message_list))
-            if re.search(r'\bpourquoi\W*$', message.content.lower()):
-                await message.channel.send(random.choice(message_list))
-
 
 # Launch the bot
 bot = MyClient(intents=intents)
